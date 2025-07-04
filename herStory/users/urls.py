21b4ce3e6dd5254b485f8a4d51579d2179b453cf
Heejin <hihj0914@gmail.com>
2025-07-04 17:21:59 +0900

    마이페이지 회원 탈퇴 구현 및 카카오 계정 이메일 정보 받는 동의 설정 완료

--- herStory/users/urls.py
+++ herStory/users/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    path('delete/', views.delete_account, name='delete_account'),
     path('mypage/', views.mypage, name='mypage'),
     path('mypage/edit/', views.edit_profile, name='edit_profile'),
     # 추후 home에 추가
