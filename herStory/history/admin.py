382ed0e500ab9aee06bf2d2c37b411a5eb58ea92
Heejin <hihj0914@gmail.com>
2025-07-05 17:30:17 +0900

    마이페이지 수정 완료 / 유저 모델 DB 수정 완료 / 여성사 카드, 퀴즈 모델 수정 완료 => migrate 필수

--- herStory/history/admin.py
+++ herStory/history/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
+from .models import HistoryCard, UserSavedCard
 
-# 컨텐츠 DB 개발 중 직접 관리하기 위해서 추가했습니다!
-from .models import HistoryContent, UserProgress
-
-admin.site.register(HistoryContent)
-admin.site.register(UserProgress)
+admin.site.register(HistoryCard)
+admin.site.register(UserSavedCard)
