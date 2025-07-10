b4c60a810397281694b0e4a65d5e544cdc40b792
chacha <0409democracy@gmail.com>
2025-07-10 20:32:30 +0900

    feat: quiz (explanation, ranking 제외) 백-프론트 연동 완료 - sqlite 제외, static 디렉터리 포함 chore: db 적용 위해 python manage.py loaddata category_fixture.json quiz_fixture.json 명령 실행 필요

---
+++ herStory/static/quizzes/js/3-3_quiz.js
@@ -0,0 +1,67 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const selctionbutton = document.querySelectorAll('.selction button');
+    const nextButton = document.querySelector('.next');
+    const unionImage = document.querySelector('.union');
+    let selectedButton = null;
+    
+    selctionbutton.forEach(button => {
+        button.addEventListener('click', function() {
+            if (selectedButton) { 
+                selectedButton.style.backgroundColor = '#f5ffef';
+            }   
+            this.style.backgroundColor = '#91C84F'; 
+            selectedButton = this;
+        });
+    });
+    
+    nextButton.addEventListener('click', function() {
+        if (!selectedButton) {
+            return; 
+        } 
+        
+        nextButton.style.backgroundColor = '#91C84F'; 
+        const unionImage = document.querySelector('.union'); // mj - 이미지 바꾸기 data-속성
+    
+            setTimeout(function() {
+            window.location.href = '4_score.html';
+        }, 200);
+    });
+      function saveAnswerAndNext(quizNum, selectedAnswer) {
+          userAnswers[quizNum] = selectedAnswer;
+          // 다음 페이지로 이동 또는 결과 계산
+      }
+
+});
+
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("menuToggle");
+  const menu = document.getElementById("menu");
+  const menuItems = menu.querySelectorAll("li");
+
+  // 햄버거 버튼 클릭 시 메뉴 토글
+  menuToggle.addEventListener("click", (e) => {
+    e.stopPropagation(); // 상위로 이벤트 전파 방지
+    menu.classList.toggle("active");
+  });
+
+  // li 클릭 시 해당 li에 active 클래스 적용
+  menuItems.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      e.stopPropagation(); // 클릭해도 메뉴가 바로 닫히지 않도록
+      menuItems.forEach((el) => el.classList.remove("active"));
+      item.classList.add("active");
+    });
+  });
+
+  // 메뉴 영역 클릭 시 이벤트 전파 방지
+  menu.addEventListener("click", (e) => {
+    e.stopPropagation();
+  });
+
+  // 메뉴 외부 클릭 시 메뉴 닫기
+  document.addEventListener("click", () => {
+    menu.classList.remove("active");
+  });
+});
