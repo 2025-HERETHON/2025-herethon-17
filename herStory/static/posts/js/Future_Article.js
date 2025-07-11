23e9ccde8e6bf2b11f9521c8c1f589bbdfa22957
Heejin <hihj0914@gmail.com>
2025-07-11 23:00:29 +0900

    css, js 전격 수정

--- herStory/static/posts/js/Future_Article.js
+++ herStory/static/posts/js/Future_Article.js
@@ -1,36 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const menuToggle = document.getElementById("menuToggle");
-  const menu = document.getElementById("menu");
-  const menuItems = menu.querySelectorAll("li");
-
-  // 햄버거 버튼 클릭 시 메뉴 토글
-  menuToggle.addEventListener("click", (e) => {
-    e.stopPropagation(); // 상위로 이벤트 전파 방지
-    menu.classList.toggle("active");
-  });
-
-  // li 클릭 시 해당 li에 active 클래스 적용
-  menuItems.forEach((item) => {
-    item.addEventListener("click", (e) => {
-      e.stopPropagation(); // 클릭해도 메뉴가 바로 닫히지 않도록
-      menuItems.forEach((el) => el.classList.remove("active"));
-      item.classList.add("active");
-    });
-  });
-
-  // 메뉴 영역 클릭 시 이벤트 전파 방지
-  menu.addEventListener("click", (e) => {
-    e.stopPropagation();
-  });
-
-  // 메뉴 외부 클릭 시 메뉴 닫기
-  document.addEventListener("click", () => {
-    menu.classList.remove("active");
-  });
-
-// 요기까지 내비바 기능
-// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-
 // ↓ 새글 작성버튼
 const New = document.querySelector('.new');
       New.addEventListener('click', function() {
@@ -38,5 +5,3 @@ const New = document.querySelector('.new');
         window.location.href = 'Future_write.html';
     }, 200);
   });
-
-});
